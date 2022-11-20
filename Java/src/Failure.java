c8c7803cab8e4faae1dc078632b55a38012993d4
sadik <sadik@LAPTOP-HMOCAJU8>
2022-11-20 18:32:14 -0500

    Fail

---
+++ Java/src/Failure.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Failure {
+
+	public static void main(String[] args) {
+		string;
+// 		String name = "jj";																								
+//		String name = "whatIsIt";
+//		System.out.print(name);
+		string Name;
+		Int pop;
+//		int top;
+		String name = 'jj';
+		System.out.print(name);
+		
+		
+	}
+
+}
