f85e133f8e6d9a48107c28bfda1fd261bd5cc668
sadik <sadikmohamed205@gmail.com>
2022-12-21 09:47:32 -0500

    Async

---
+++ Async/Await/FetchRemoteData.js
@@ -0,0 +1,9 @@
+async function fetchCityData(city) {
+    const response = await fetch(
+      `https://geocode.xyz/${city}?json=1`
+    );
+    const data = await response.json();
+    console.log(data.latt, data.longt);
+   }
+   fetchCityData("Minsk");
+   fetchCityData("Belgrade");
