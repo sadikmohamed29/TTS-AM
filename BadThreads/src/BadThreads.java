727a5822cf5c6f15b9e4a6f7806ee0a8641d0f66
sadik <sadikmohamed205@gmail.com>
2022-12-25 02:57:33 -0500

    Concurrency

--- BadThreads/src/BadThreads.java
+++ BadThreads/src/BadThreads.java
@@ -45,3 +45,13 @@ static String message;
         System.out.println(message);
 }
 }
+
+//2. It is always guaranteed to do so.
+
+//-  However, this result is not guaranteed, because there is no happens-before relationship between "Key statement 1" and "Key statement 2". This is true even if "Key statement 1" actually executes before "Key statement 2" — remember, a happens-before relationship is about visibility, not sequence.
+
+//3. Changing the arguments of the two sleep invocations does not help either, since this does nothing to guarantee a happens-before relationship.
+
+
+//4.  In the main thread, retain a reference to the CorrectorThread instance. Then invoke join on that instance before referring to message
+//Encapsulate message in an object with synchronized methods. Never reference message except through those methods.
